9f6c3b21913b088a66bfa455fc9dc24aa555ed07
Worro <alessiodelledonne29@gmail.com>
2024-06-09 18:56:56 +0200

    app deployed

---
+++ src/main/java/com/example/demo/DevController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DevController {
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${CustomVarTree.devName}")
+    String devName;
+
+
+    @GetMapping
+    public String devName(){
+        return devName;
+    }
+}
